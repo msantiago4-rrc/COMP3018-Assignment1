633441a1607a36296a65446468f744c468750754
Matthew Santiago <msantiago4@academic.rrc.ca>
2026-09-23 16:13:36 -0500

    feat: added test when percentChange is between 10-30%

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -15,3 +15,19 @@ describe("calculatePortfolioPerformance", () => {
         expect(result?.performanceSummary).toBe("Excellent performance! Your investments are doing great.")
     })
 })
+
+describe("calculatePortfolioPerformance", () => {
+    it("Should return the correct string when percentChange is < 30 & >= 10 ", () => {
+        // ARRANGE: Set up conditions
+        const initialInvestment = 10000;
+        const currentValue = 12999.9
+
+        // ACT: Call the function
+        const result = calculatePortfolioPerformance(initialInvestment, currentValue);
+
+        // ASSERT: Verify the results
+        expect(result).not.toBeNull();
+        expect(result?.percentageChange).toBe(29.999)
+        expect(result?.performanceSummary).toBe("Solid gain. Keep monitoring your investments")
+    })
+})
