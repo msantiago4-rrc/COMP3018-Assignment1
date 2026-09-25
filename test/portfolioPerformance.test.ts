dc6fe813d867d402c9b0a69160902b114673512a
Matthew Santiago <msantiago4@academic.rrc.ca>
2026-09-25 04:14:25 -0500

    feat: added test case when portfolio has suffered minor loss.

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -47,3 +47,19 @@ describe("calculatePortfolioPerformance", () => {
         expect(result?.performanceSummary).toBe("No change. Your portfolio is holding steady.")
     })
 })
+
+describe("calculatePortfolioPerformance", () => {
+    it("Should return the correct string when percentChange is in between 0 and -10", () => {
+        // ARRANGE: Set up conditions
+        const initialInvestment = 10000;
+        const currentValue = 9000;
+
+        // ACT: Call the function
+        const result = calculatePortfolioPerformance(initialInvestment, currentValue);
+
+        // ASSERT: Verify the results
+        expect(result).not.toBeNull();
+        expect(result?.percentageChange).toBe(-10)
+        expect(result?.performanceSummary).toBe("Minor loss. Stay calm and review your options.")
+    })
+})
