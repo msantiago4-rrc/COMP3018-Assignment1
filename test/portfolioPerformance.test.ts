0848fb01bbb45addf02f6dfc76a77c12a90058b1
Matthew Santiago <msantiago4@academic.rrc.ca>
2026-09-25 04:03:44 -0500

    feat: added test case for when there is no change to the portfolio

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -31,3 +31,19 @@ describe("calculatePortfolioPerformance", () => {
         expect(result?.performanceSummary).toBe("Solid gain. Keep monitoring your investments")
     })
 })
+
+describe("calculatePortfolioPerformance", () => {
+    it("Should return the correct string when percentChange is 0.", () => {
+        // ARRANGE: Set up conditions
+        const initialInvestment = 10000;
+        const currentValue = 10000;
+
+        // ACT: Call the function
+        const result = calculatePortfolioPerformance(initialInvestment, currentValue);
+
+        // ASSERT: Verify the results
+        expect(result).not.toBeNull();
+        expect(result?.percentageChange).toBe(0)
+        expect(result?.performanceSummary).toBe("No change. Your portfolio is holding steady.")
+    })
+})
